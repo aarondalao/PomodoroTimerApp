d91c1f76376d6bf97348503c313358a598263aaa
aaron_dalao <aaron.dalao@gmail.com>
2021-06-14 07:03:22 +0800

    Add project files.

---
+++ PomodoroTimerApp/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace PomodoroTimerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timeOutputLabel = new System.Windows.Forms.Label();
+            this.pomodoroButton = new System.Windows.Forms.Button();
+            this.shortBreakButton = new System.Windows.Forms.Button();
+            this.longBreakButton = new System.Windows.Forms.Button();
+            this.startStopButton = new System.Windows.Forms.Button();
+            this.PTimer = new System.Windows.Forms.Timer(this.components);
+            this.STimer = new System.Windows.Forms.Timer(this.components);
+            this.LTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(756, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reportToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // reportToolStripMenuItem
+            // 
+            this.reportToolStripMenuItem.Name = "reportToolStripMenuItem";
+            this.reportToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.reportToolStripMenuItem.Text = "Report";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // timeOutputLabel
+            // 
+            this.timeOutputLabel.AutoSize = true;
+            this.timeOutputLabel.Font = new System.Drawing.Font("Microsoft Yi Baiti", 99F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeOutputLabel.Location = new System.Drawing.Point(143, 153);
+            this.timeOutputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timeOutputLabel.Name = "timeOutputLabel";
+            this.timeOutputLabel.Size = new System.Drawing.Size(143, 165);
+            this.timeOutputLabel.TabIndex = 1;
+            this.timeOutputLabel.Text = " ";
+            // 
+            // pomodoroButton
+            // 
+            this.pomodoroButton.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pomodoroButton.Location = new System.Drawing.Point(11, 83);
+            this.pomodoroButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pomodoroButton.Name = "pomodoroButton";
+            this.pomodoroButton.Size = new System.Drawing.Size(234, 48);
+            this.pomodoroButton.TabIndex = 2;
+            this.pomodoroButton.Text = "Pomodoro";
+            this.pomodoroButton.UseVisualStyleBackColor = true;
+            this.pomodoroButton.Click += new System.EventHandler(this.PomodoroButton_Click);
+            // 
+            // shortBreakButton
+            // 
+            this.shortBreakButton.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shortBreakButton.Location = new System.Drawing.Point(253, 83);
+            this.shortBreakButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.shortBreakButton.Name = "shortBreakButton";
+            this.shortBreakButton.Size = new System.Drawing.Size(244, 48);
+            this.shortBreakButton.TabIndex = 3;
+            this.shortBreakButton.Text = "Short Break";
+            this.shortBreakButton.UseVisualStyleBackColor = true;
+            this.shortBreakButton.Click += new System.EventHandler(this.shortBreakButton_Click);
+            // 
+            // longBreakButton
+            // 
+            this.longBreakButton.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.longBreakButton.Location = new System.Drawing.Point(505, 83);
+            this.longBreakButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.longBreakButton.Name = "longBreakButton";
+            this.longBreakButton.Size = new System.Drawing.Size(234, 48);
+            this.longBreakButton.TabIndex = 4;
+            this.longBreakButton.Text = "Long Break";
+            this.longBreakButton.UseVisualStyleBackColor = true;
+            this.longBreakButton.Click += new System.EventHandler(this.longBreakButton_Click);
+            // 
+            // startStopButton
+            // 
+            this.startStopButton.Font = new System.Drawing.Font("Microsoft Yi Baiti", 45F);
+            this.startStopButton.Location = new System.Drawing.Point(11, 341);
+            this.startStopButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startStopButton.Name = "startStopButton";
+            this.startStopButton.Size = new System.Drawing.Size(728, 169);
+            this.startStopButton.TabIndex = 5;
+            this.startStopButton.Text = "Start";
+            this.startStopButton.UseVisualStyleBackColor = true;
+            this.startStopButton.Click += new System.EventHandler(this.startStopButton_Click);
+            // 
+            // PTimer
+            // 
+            this.PTimer.Interval = 1000;
+            this.PTimer.Tick += new System.EventHandler(this.PTimer_Tick);
+            // 
+            // STimer
+            // 
+            this.STimer.Interval = 1000;
+            this.STimer.Tick += new System.EventHandler(this.STimer_Tick);
+            // 
+            // LTimer
+            // 
+            this.LTimer.Interval = 1000;
+            this.LTimer.Tick += new System.EventHandler(this.LTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 525);
+            this.Controls.Add(this.startStopButton);
+            this.Controls.Add(this.longBreakButton);
+            this.Controls.Add(this.shortBreakButton);
+            this.Controls.Add(this.pomodoroButton);
+            this.Controls.Add(this.timeOutputLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Pomodoro Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label timeOutputLabel;
+        private System.Windows.Forms.Button pomodoroButton;
+        private System.Windows.Forms.Button shortBreakButton;
+        private System.Windows.Forms.Button longBreakButton;
+        private System.Windows.Forms.Button startStopButton;
+        private System.Windows.Forms.Timer PTimer;
+        private System.Windows.Forms.Timer STimer;
+        private System.Windows.Forms.Timer LTimer;
+    }
+}
+
---
+++ PomodoroTimerApp/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PomodoroTimerApp
+{
+    public partial class Form1 : Form
+    {
+        private int ss, mm, savedSeconds, savedMinutes;
+
+        // buttons to choose which timer will choose. this would set and the coresponding minutes for each individual timers.
+        private bool PButtonClicked = false, SButtonClicked = false, LButtonClicked = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+     
+        }
+
+        // timer_ticks methods
+
+        private void PTimer_Tick(object sender, EventArgs e)
+        {
+            
+            if (ss > 0)
+            {
+                ss--;
+                timeOutputLabel.Text = mm.ToString() + ":" + ss.ToString();
+                savedSeconds = ss;
+            }
+            else
+            {
+                if (mm > 0)
+                {
+                    mm--;
+                    ss = 60;            // change this for faster debugging!
+                    timeOutputLabel.Text = mm.ToString() + ":" + ss.ToString();
+                    savedMinutes = mm;
+                    savedSeconds = ss;
+                }
+                else
+                {
+                    //this.tabControl1.SelectedTab = Short_Break;
+
+                    timeOutputLabel.ForeColor = System.Drawing.Color.Green;
+                    timeOutputLabel.Text = "break time!";
+
+                    PTimer.Stop();
+                    PTimer.Dispose();
+                    PTimer.Enabled = false;
+
+                    //startStopButton
+                }
+            }
+        }
+        private void STimer_Tick(object sender, EventArgs e)
+        {
+            if (ss > 0)
+            {
+                ss--;
+                timeOutputLabel.Text = mm.ToString() + ":" + ss.ToString();
+                savedSeconds = ss;
+            }
+            else
+            {
+                if (mm > 0)
+                {
+                    mm--;
+                    ss = 60;            // change this for faster debugging!
+                    timeOutputLabel.Text = mm.ToString() + ":" + ss.ToString();
+                    savedMinutes = mm;
+                    savedSeconds = ss;
+                }
+                else
+                {
+                    //this.tabControl1.SelectedTab = Short_Break;
+
+                    timeOutputLabel.ForeColor = System.Drawing.Color.Green;
+                    timeOutputLabel.Text = "break time!";
+
+                    PTimer.Stop();
+                    PTimer.Dispose();
+                    PTimer.Enabled = false;
+
+                    //startStopButton
+                }
+            }
+        }
+        private void LTimer_Tick(object sender, EventArgs e)
+        {
+            if (ss > 0)
+            {
+                ss--;
+                timeOutputLabel.Text = mm.ToString() + ":" + ss.ToString();
+                savedSeconds = ss;
+            }
+            else
+            {
+                if (mm > 0)
+                {
+                    mm--;
+                    ss = 60;            // change this for faster debugging!
+                    timeOutputLabel.Text = mm.ToString() + ":" + ss.ToString();
+                    savedMinutes = mm;
+                    savedSeconds = ss;
+                }
+                else
+                {
+                    //this.tabControl1.SelectedTab = Short_Break;
+
+                    timeOutputLabel.ForeColor = System.Drawing.Color.Green;
+                    timeOutputLabel.Text = "break time!";
+
+                    PTimer.Stop();
+                    PTimer.Dispose();
+                    PTimer.Enabled = false;
+
+                    //startStopButton
+                }
+            }
+        }
+
+
+        //
+        //  start/stop button clicks
+        //
+
+
+        private void PomodoroButton_Click(object sender, EventArgs e)
+        {
+            // designs for the labels and the back color of the form
+            timeOutputLabel.Text = "24:00";
+            this.BackColor = Color.FromArgb(219, 82, 77);
+
+            PButtonClicked = true;
+            ss = 60;
+            mm = 1;
+        }
+
+        private void shortBreakButton_Click(object sender, EventArgs e)
+        {
+            timeOutputLabel.Text = "5:00";
+            this.BackColor = Color.FromArgb(70, 142, 145);
+
+            SButtonClicked = true;
+
+            ss = 60;
+            mm = 1;
+           
+
+        }
+        private void longBreakButton_Click(object sender, EventArgs e)
+        {
+            timeOutputLabel.Text = "15:00";
+            this.BackColor = Color.FromArgb(67, 126, 168);
+
+            LButtonClicked = true;
+
+            ss = 60;
+            mm = 1;
+
+        }
+
+
+        // start stop button
+        private void startStopButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                    if (PButtonClicked == true)
+                    {
+                        startStopButton.Text = "Stop";
+                        CreatePomodoroTimer();
+                        PTimer.Start();
+                        PButtonClicked = false;
+
+                        pomodoroButton.Enabled = false;
+                        shortBreakButton.Enabled = false;
+                        longBreakButton.Enabled = false;
+                    }
+                    else if (PButtonClicked == false)
+                    {
+                        PTimer.Stop();
+                        startStopButton.Text = "Start";
+                        PButtonClicked = true;
+
+                        pomodoroButton.Enabled = true;
+                        shortBreakButton.Enabled = true;
+                        longBreakButton.Enabled = true;
+                    }
+                    else if (SButtonClicked == true)
+                    {
+                        startStopButton.Text = "Stop";
+                        CreateShortBreakTimer();
+                        STimer.Start();
+                        SButtonClicked = false;
+
+                        pomodoroButton.Enabled = false;
+                        shortBreakButton.Enabled = false;
+                        longBreakButton.Enabled = false;
+                    }
+                    else if (SButtonClicked == false)
+                    {
+                        STimer.Stop();
+                        startStopButton.Text = "Start";
+                        SButtonClicked = true;
+
+                        pomodoroButton.Enabled = true;
+                        shortBreakButton.Enabled = true;
+                        longBreakButton.Enabled = true;
+                    }
+
+                    else if (LButtonClicked == true)
+                    {
+                        startStopButton.Text = "Stop";
+                        CreateLongBreakTimer();
+                        LTimer.Start();
+                        LButtonClicked = false;
+
+                        pomodoroButton.Enabled = false;
+                        shortBreakButton.Enabled = false;
+                        longBreakButton.Enabled = false;
+                    }
+                    else if (LButtonClicked == false) {
+                        LTimer.Stop();
+                        startStopButton.Text = "Start";
+                        LButtonClicked = true;
+
+                        pomodoroButton.Enabled = true;
+                        shortBreakButton.Enabled = true;
+                        longBreakButton.Enabled = true;
+                    }
+      
+            }
+            catch (System.IO.IOException x)
+            {
+                Console.WriteLine(x.HResult);
+                Console.WriteLine(x.Message);
+                Console.WriteLine(x.StackTrace);
+            }
+        }
+
+        
+
+        // on form load
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PButtonClicked = true;
+            timeOutputLabel.Text = "24:00";
+            this.BackColor = Color.FromArgb(219, 82, 77);
+
+            ss = 60;
+            mm = 1;
+        }
+
+
+        private void CreatePomodoroTimer() {
+            // create an instance of an object PTimer
+            Timer PTimer = new Timer();
+
+            // add the event Tick and the event handler for the method that will process the timer event to the timer
+            PTimer.Tick += new EventHandler(PTimer_Tick);
+            // set interval
+
+            PTimer.Interval = 1000;
+            //this.ss = setSeconds(ss); // set the value of seconds inside of this constructor 
+            //this.mm = setMinutes(mm); // set the value of minutes inside of this constructor named Form1
+          
+        }
+
+        private void CreateShortBreakTimer() {
+            // create an instance of an object PTimer
+            Timer STimer = new Timer();
+
+            // add the event Tick and the event handler for the method that will process the timer event to the timer
+            PTimer.Tick += new EventHandler(STimer_Tick);
+            // set interval
+
+            PTimer.Interval = 1000;
+        }
+
+        private void CreateLongBreakTimer() {
+            // create an instance of an object PTimer
+            Timer LTimer = new Timer();
+
+            // add the event Tick and the event handler for the method that will process the timer event to the timer
+            PTimer.Tick += new EventHandler(LTimer_Tick);
+            // set interval
+
+            PTimer.Interval = 1000;
+        }
+    }
+}
